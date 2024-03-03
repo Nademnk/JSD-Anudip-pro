ee11c7b0d7fcf93804a84e5426a5e27ca8cff4a7
Nadeem Khan <103941819+Nademnk@users.noreply.github.com>
2024-03-03 23:31:57 +0530

    deleted

--- Blue Chem-Sprint 1/ Blue Chem/src/main/java/com/BlueChem/sbtp/repository/OrderMdcnRespository.java
+++
@@ -1,9 +0,0 @@
-package com.BlueChem.sbtp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.BlueChem.sbtp.entity.OrderMdcn;
-
-public interface OrderMdcnRespository  extends JpaRepository<OrderMdcn, Integer>{
-
-}
--- Blue Chem-Sprint 1/ Blue Chem/src/main/java/com/BlueChem/sbtp/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.BlueChem.sbtp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.BlueChem.sbtp.entity.PaymentDetails;
-
-public interface PaymentRepository  extends JpaRepository<PaymentDetails, Integer>{
-	
-}

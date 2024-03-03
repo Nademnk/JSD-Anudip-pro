05fa57d702db693b72876f6b4ed59bf10157f4e6
Nadeem Khan <103941819+Nademnk@users.noreply.github.com>
2024-03-03 23:57:37 +0530

    Delete

---  Blue Chem/src/main/java/com/BlueChem/sbtp/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.BlueChem.sbtp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.BlueChem.sbtp.entity.Cart;
-
-public interface CartRepository extends JpaRepository<Cart, Integer>{
-
-}
